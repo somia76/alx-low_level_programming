ce6fd0be5f98a3ebace5f60b7a1530a3773b39e1
somia76 <somiaahmed566@gmail.com>
2023-03-28 12:27:26 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+#include <stdio.h>
+
+void positive_or_negative(int);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
